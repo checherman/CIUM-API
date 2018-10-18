bec1bc1552e27670a262bcf6f5413014baf9a8a3
Eliecer Ramirez Esquinca <ramirez.esquinca@gmail.com>
2018-10-18 13:33:57 -0500

    CIUM WEB con las evaluaciones de PROCESO, RECURSO y PC, graficos mejorados

--- database/migrations/2018_10_15_121856_add_foreign_keys_to_CriterioValidacionRespuesta_table.php
+++ database/migrations/2018_10_15_121856_add_foreign_keys_to_CriterioValidacionRespuesta_table.php
@@ -15,7 +15,7 @@ class AddForeignKeysToCriterioValidacionRespuestaTable extends Migration {
 		Schema::table('CriterioValidacionRespuesta', function(Blueprint $table)
 		{
 			$table->foreign('idCriterio', 'fk_criterio_criterio_validacion_respuesta')->references('id')->on('Criterio')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-			$table->foreign('idCriterioValidacion', 'fk_criterio_validacion_criterio_validacion_respuesta')->references('id')->on('Criteriovalidacion')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+			$table->foreign('idCriterioValidacion', 'fk_criterio_validacion_criterio_validacion_respuesta')->references('id')->on('CriterioValidacion')->onUpdate('NO ACTION')->onDelete('NO ACTION');
 		});
 	}
 
